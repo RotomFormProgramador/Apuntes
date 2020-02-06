f2deb0b6740ec412f0f110cbab38078b6f29b061
RotomFormProgramador <59156337+RotomFormProgramador@users.noreply.github.com>
2020-02-05 22:34:56 -0600

    Material Desing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import './App.css';
-import { Navbar, Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar,IconButton,Typography,Button,SvgIcon } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import menu from './menu.svg'
+
+function MenuIcon(props) {
+    return (
+      <SvgIcon {...props}>
+         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+      </SvgIcon>
+    );
+  }
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar bg="dark" variant="dark"  >
-                <Navbar.Brand  as={Link} to="/">
-                    React-Bootstrap
-                </Navbar.Brand>
+    const classes = useStyles();    
 
-                <Nav className="mr-auto">
-                    <Nav.Link className="navegador" as={Link} to="/About">About</Nav.Link>
-                    <Nav.Link as={Link} to="/Support">Suport</Nav.Link>
-
-                </Nav>
-
-            </Navbar>
-        </div>
-    );
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon color="light" />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default App;
