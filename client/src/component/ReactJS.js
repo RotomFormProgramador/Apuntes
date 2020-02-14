fdad097a104215b6e7501fc517d07d65ba3aacc3
RotomFormProgramador <juniorbenjamin12@hotmail.com>
2020-02-14 01:48:08 +0000

    Primeros Pasos

--- client/src/component/ReactJS.js
+++ client/src/component/ReactJS.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import './css/ReactJS.css'
-import {Typography} from "@material-ui/core"
-import {Link,Switch,Route,useRouteMatch} from "react-router-dom"
+import { Typography } from "@material-ui/core"
+import { Link, Switch, Route, useRouteMatch } from "react-router-dom"
 import Introduccion from "./document/reactjs/Introduccion"
 import PrimerosPasos from "./document/reactjs/PrimerosPasos"
 
 
 
-export default function ReactJS (){
-    let {path,url} =useRouteMatch()
-    return(
+export default function ReactJS() {
+    let { path, url } = useRouteMatch()
+    return (
         <div className="ReactJS">
-        <div className="content">
-            <Typography   style={{marginTop:"15px"}} variant="h6" align="center">
-                ReactJs
+            <div className="content">
+                <Typography style={{ marginTop: "15px" }} variant="h6" align="center">
+                    ReactJs
             </Typography>
-            <Typography  variant="subtitle1" align="center">
-            <Link className="indice" to={`${url}`}>
-            Introduccion
+                <Typography variant="subtitle1" align="center">
+                    <Link className="indice" to={`${url}`}>
+                        Introduccion
             </Link>
-            <Link className="indice" to={`${url}/PrimerosPasos`}>Primeros Pasos</Link>
-            </Typography>
+                    <Link className="indice" to={`${url}/PrimerosPasos`}>Primeros Pasos</Link>
+                </Typography>
             </div>
 
-        <div className="documentation">
-        <Switch>
-            <Route exact path={path}><Introduccion/></Route>
-            <Route path={`${path}/PrimerosPasos`}><PrimerosPasos/></Route>
-        </Switch>
-        </div>
+            <div className="documentation">
+                <Switch>
+                    <Route exact path={path}><Introduccion /></Route>
+                    <Route path={`${path}/PrimerosPasos`}><PrimerosPasos /></Route>
+                </Switch>
+            </div>
 
         </div>
     )
--- client/src/component/document/reactjs/PrimerosPasos.js
+++ client/src/component/document/reactjs/PrimerosPasos.js
@@ -4,11 +4,18 @@ import   "./Index.css"
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
-const Prueva = () => {
-  const codeString = '(num) => num + 1';
+
+
+const sourceC = {
+    crearAPP:'npm create-react-app my-name-app',
+    startSer:'npm start'
+};
+
+const Hcode = (props) => {
+  
   return (
     <SyntaxHighlighter language="javascript" style={tomorrow}>
-      {codeString}
+      {props.code}
     </SyntaxHighlighter>
   );
 };
@@ -23,17 +30,55 @@ function PrimerosPasos (){
         Primeros Pasos
         </Typography> 
         <br/>
+
+        <Typography display="block"   variant="h5">
+        Creacion de aplicacion de React
+        </Typography>
         <div className="cont-1">
         <Typography display="block" align="justify" variant="body1">
-        
+        La forma mas facil de comenzar con react es creando un aplicacion de react y esto se logro con el siguiente comando en la consola
         </Typography>
         
         </div>
-        <Prueva/>
-         
+        <Hcode code={sourceC.crearAPP}/>
+
+        <Typography display="block" align="justify" variant="body1">
+        Al ejecutar este comando se creara un proyecto que contine todas
+        las herramientas de react, las configuraciones nesesarias para el
+        funcionamiento (wedpack), y contiene un pequeño proyeto para comenzar a modificar.
+        </Typography>
+
+        <br/>
+         <Typography display="block" align="justify" variant="body1">
+         El archivo contiene tu aplicacion en App.js  que esta ubicado en src/App.js desde
+         aqui puedes comenzar  a crear  tu proyecto. Este archivo ya contiene un componente hecho,
+         puedes borrar parte de este componete pero deja el div que tiene la clase #App, ya que en este
+         div se contruye tu aplicacion.
+
+         </Typography>
+        <br/>
+
+        <Typography display="block"   variant="h5">
+        Creacion de server para desarrollo
+        </Typography>
+
+        <Typography display="block" align="justify" variant="body1">
+        Se puede crear facilmente un servidor que ejecuta tu aplicacion con  el siguiente comando 
+        </Typography>
+
+        <Hcode code={sourceC.startSer} />
+
+        <br/>
+        <br/>
+        <br/>
+
+
+
         </div>
     
     )
 }
 
 export default PrimerosPasos;
+
+
